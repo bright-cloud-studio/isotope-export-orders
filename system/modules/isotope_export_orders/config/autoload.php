ab93ac4eeb978c5d54ab14532f7cf98810062460
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-04 15:38:26 -0500

    Create autoload.php

---
+++ system/modules/isotope_export_orders/config/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Bright Cloud Studio's Isotope BOGO
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-bogo
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
+/* Register Classes */
+ClassLoader::addClasses(array
+(
+    // Class that contains all of our functionality for the Isotope Hooks
+    'Bcs\Backend\Export'         => 'system/modules/isotope_export_orders/library/Bcs/Backend/Export.php'
+));
