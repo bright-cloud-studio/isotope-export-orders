f6d7badb582fc05ce8411a1ba95ac5b7a80dd320
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-05 09:58:09 -0500

    Create tl_iso_product_collection.php

---
+++ system/modules/isotope_export_orders/languages/en/tl_iso_product_collection.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope Export Orders
+ *
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-export-orders
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Buttons */
+$GLOBALS['TL_LANG']['tl_iso_product_collection']['export_all_orders']   = "Export to CSV";
