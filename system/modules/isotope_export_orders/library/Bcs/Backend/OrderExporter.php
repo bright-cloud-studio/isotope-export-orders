89279d745a25e8cb84b7e4f07442ee5f55493070
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-05 13:36:28 -0500

    Update OrderExporter.php

--- system/modules/isotope_export_orders/library/Bcs/Backend/OrderExporter.php
+++ system/modules/isotope_export_orders/library/Bcs/Backend/OrderExporter.php
@@ -46,64 +46,14 @@ class OrderExporter {
         $this->logger = System::getContainer()->get('monolog.logger.contao');
         $this->currentRequestUrl = Environment::get('request');
     }
-
-
-     public function exportToCSV() {
-         
-        $this->redirectUrl = $this->getRedirectUrl();
-
-        // If form id is available, the form has been sent.
-        if (Input::post('FORM_SUBMIT') === self::EXPORT_TO_CSV_FROM_ID) {
-            echo "exportToCSV: 1 <br>";
-        }
-        else {
-            /** @var \Contao\SelectMenu $selectMenu */
-            $selectMenu = $this->getDocumentTypeSelectMenu();
-            $messages = Message::generate();
-            Message::reset();
-
-            // TODO: use better solution as this Contao default.
-            // Return form.
-            return '
-<div id="tl_buttons">
- <a href="' . ampersand($this->redirectUrl) . '" class="header_back" title="' . StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['backBT']) . '">' . $GLOBALS['TL_LANG']['MSC']['backBT'] . '</a>
-</div>
-
-<h2 class="sub_headline">' . $GLOBALS['TL_LANG']['tl_iso_product_collection']['print_all_documents'][0] . '</h2>' . $messages . '
-
-<form action="' . ampersand($this->currentRequestUrl, true) . '" id="' . self::PRINT_ALL_DOCUMENTS_FROM_ID . '" class="tl_form" method="post">
-    <div class="tl_formbody_edit">
-        <input type="hidden" name="FORM_SUBMIT" value="' . self::PRINT_ALL_DOCUMENTS_FROM_ID . '">
-        <input type="hidden" name="REQUEST_TOKEN" value="' . REQUEST_TOKEN . '">
-        
-        <div class="tl_tbox block">
-          <div class="clr widget">
-            ' . $selectMenu->parse() . '
-            <p class="tl_help">' . $selectMenu->description . '</p>
-          </div>
-        </div>
-    </div>
-    <div class="tl_formbody_submit">
-        <div class="tl_submit_container">
-            <input type="submit" name="print" id="print" class="tl_submit" alt="" accesskey="s" value="' . StringUtil::specialchars($GLOBALS['TL_LANG']['tl_iso_product_collection']['print']) . '">
-        </div>
-    </div>
-</form>';
-        }
-
+    
+    
+    public function exportToCSV() {
+        echo "FUNCTION HIT!";
+        die();
+    
     }
 
 
-     protected function getDocumentTypeSelectMenu(): SelectMenu {
-        $selectData = [
-            'name' => 'document',
-            'label' => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['document_choice'],
-            'inputType' => 'select',
-            'foreignKey' => 'tl_iso_document.name',
-            'eval' => ['mandatory' => TRUE]
-        ];
-
-        return new SelectMenu(SelectMenu::getAttributesFromDca($selectData, $selectData['name']));
-    }
 
 }
