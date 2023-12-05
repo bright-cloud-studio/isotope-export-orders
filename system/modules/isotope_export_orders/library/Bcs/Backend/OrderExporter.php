136b14c867b8fe44efd491772cdbb7b0f4b14f60
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-05 11:47:38 -0500

    Update OrderExporter.php

--- system/modules/isotope_export_orders/library/Bcs/Backend/OrderExporter.php
+++ system/modules/isotope_export_orders/library/Bcs/Backend/OrderExporter.php
@@ -29,9 +29,9 @@ class OrderExporter {
     protected const EXPORT_TO_CSV_FROM_ID = 'isotope_export_to_csv';
 
     /** The 'print all documents' button id */
-    public const XPORT_TO_CSV_BUTTON_ID = 'export_to_csv';
+    public const EXPORT_TO_CSV_BUTTON_ID = 'export_to_csv';
 
     /** The 'print all documents' action name */
-    public const XPORT_TO_CSV_ACTION_NAME = 'exportToCSV';
+    public const EXPORT_TO_CSV_ACTION_NAME = 'exportToCSV';
 
 }
