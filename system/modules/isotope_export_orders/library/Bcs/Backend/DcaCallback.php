ca9e5c722cc9cdd90ecb0085e1d3954bc6d1039e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-05 10:46:12 -0500

    Update DcaCallback.php

--- system/modules/isotope_export_orders/library/Bcs/Backend/DcaCallback.php
+++ system/modules/isotope_export_orders/library/Bcs/Backend/DcaCallback.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Bright Cloud Studio's Isotope Export Orders
+ *
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-export-orders
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
 namespace Bcs\Backend;
 
 use Contao\Backend;
@@ -7,21 +17,18 @@ use Contao\DC_Table;
 use Contao\Environment;
 use Contao\Input;
 
-class DcaCallback extends Backend {
+class DcaCallback extends Backend
+{
 
-    /**
-     * Called by onload_callback.
-     *
-     * @param \Contao\DC_Table $dataContainer
-     *   The Contao data container (DC).
-     */
+    // Called when clicking the "Export to CSV" button in "editAll" mode within the Orders page
     public function onLoad(DC_Table $dataContainer): void {
+        
         // Is available after button submission.
         if (Input::post('FORM_SUBMIT') === 'tl_select') {
             echo "BING BONG";
             die();
         }
-
+    
     }
 
 }
