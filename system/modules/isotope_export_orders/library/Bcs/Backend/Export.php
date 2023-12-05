c6f42fea424d9ed5c0f539f75ac2bfaf9f89cee6
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-05 10:46:57 -0500

    Update Export.php

--- system/modules/isotope_export_orders/library/Bcs/Backend/Export.php
+++ system/modules/isotope_export_orders/library/Bcs/Backend/Export.php
@@ -7,7 +7,7 @@ use Contao\StringUtil;
 class Export
 {
     public function addExportButton(array $buttons): array {
-        $buttons['export_all_orders'] = '<button type="submit" name="export_all_orders" id="export_all_orders" class="tl_submit">asdf ' . StringUtil::specialchars($GLOBALS['TL_LANG']['tl_iso_product_collection']['export']) . '</button> ';
+        $buttons['export_all_orders'] = '<button type="submit" name="export_all_orders" id="export_all_orders" class="tl_submit">asdf ' . StringUtil::specialchars($GLOBALS['TL_LANG']['tl_iso_product_collection']['export_all_orders']) . '</button> ';
         return $buttons;
     }
 }
