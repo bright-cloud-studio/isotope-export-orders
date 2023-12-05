532ab8065f5c40988fe03a1e8e9f6c5af58c298d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-05 13:57:43 -0500

    Update tl_iso_product_collection.php

--- system/modules/isotope_export_orders/dca/tl_iso_product_collection.php
+++ system/modules/isotope_export_orders/dca/tl_iso_product_collection.php
@@ -1,5 +1,7 @@
 <?php
 
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['config']['dataContainer'] = 'TableExtension';
+
 /* Callbacks */
 $GLOBALS['TL_DCA']['tl_iso_product_collection']['config']['onload_callback'][] = array('Bcs\Backend\DcaCallback', 'onLoad');
 $GLOBALS['TL_DCA']['tl_iso_product_collection']['select']['buttons_callback'][] = array('Bcs\Backend\ButtonCallback', 'addExportButton');
