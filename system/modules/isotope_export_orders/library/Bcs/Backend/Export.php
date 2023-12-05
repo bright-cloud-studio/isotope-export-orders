dffe838bf8498da7c5d098e649d52ccfb542876a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-05 10:50:15 -0500

    Update Export.php

--- system/modules/isotope_export_orders/library/Bcs/Backend/Export.php
+++ system/modules/isotope_export_orders/library/Bcs/Backend/Export.php
@@ -1,13 +1,26 @@
 <?php
 
+/**
+ * Bright Cloud Studio's Isotope Export Orders
+ *
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-export-orders
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
 namespace Bcs\Backend;
 
 use Contao\StringUtil;
 
 class Export
 {
+
+    // Adds the "Export to CSV" button to the "editAll" mode on the 'Orders' page
     public function addExportButton(array $buttons): array {
-        $buttons['export_all_orders'] = '<button type="submit" name="export_all_orders" id="export_all_orders" class="tl_submit">asdf ' . StringUtil::specialchars($GLOBALS['TL_LANG']['tl_iso_product_collection']['export_all_orders']) . '</button> ';
+        $buttons['export_all_orders'] = '<button type="submit" name="export_all_orders" id="export_all_orders" class="tl_submit">' . StringUtil::specialchars($GLOBALS['TL_LANG']['tl_iso_product_collection']['export_all_orders']) . '</button> ';
         return $buttons;
     }
+    
 }
