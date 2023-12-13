f5e7910c11fefd6ef0e6638abc79d3cb1a19abe8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-13 15:26:51 -0500

    Update tl_order_export.php

--- system/modules/isotope_export_orders/languages/en/tl_order_export.php
+++ system/modules/isotope_export_orders/languages/en/tl_order_export.php
@@ -8,6 +8,7 @@ $GLOBALS['TL_LANG']['tl_order_export']['product_legend'] 	= 'Product Settings';
 /* Config Fields */
 $GLOBALS['TL_LANG']['tl_order_export']['config_name']  = array('Configuration Name', 'Enter the name for this configuration');
 $GLOBALS['TL_LANG']['tl_order_export']['csv_filename'] = array('CSV Filename', 'Enter the filename for the exported CSV file');
+$GLOBALS['TL_LANG']['tl_order_export']['include_headers'] = array('Include Headers', 'Include a header row in the exported CSV file');
 
 /* Order Fields */
 $GLOBALS['TL_LANG']['tl_order_export']['order_id']      = array('Order ID', 'Include the "Order ID" field in the exported file');
