c032d7c5dd497a5ec87d55baa55dffe7d8964e27
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-13 15:41:08 -0500

    Update autoload.php

--- system/modules/isotope_export_orders/config/autoload.php
+++ system/modules/isotope_export_orders/config/autoload.php
@@ -15,5 +15,6 @@ ClassLoader::addClasses(array
     // Class that contains all of our functionality for the Isotope Hooks
     'Bcs\Backend\ButtonCallback'        => 'system/modules/isotope_export_orders/library/Bcs/Backend/ButtonCallback.php',
     'Bcs\Backend\DcaCallback'           => 'system/modules/isotope_export_orders/library/Bcs/Backend/DcaCallback.php',
-    'Bcs\Backend\OrderExporter'         => 'system/modules/isotope_export_orders/library/Bcs/Backend/OrderExporter.php'
+    'Bcs\Backend\OrderExporter'         => 'system/modules/isotope_export_orders/library/Bcs/Backend/OrderExporter.php',
+    'Bcs\Model\OrderExport'         => 'system/modules/isotope_export_orders/library/Bcs/Model/OrderExport.php'
 ));
