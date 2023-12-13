748cf4ad9779d4aff59bc25e2b77ee65f368b625
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-13 15:41:28 -0500

    Update config.php

--- system/modules/isotope_export_orders/config/config.php
+++ system/modules/isotope_export_orders/config/config.php
@@ -4,3 +4,6 @@ $GLOBALS['BE_MOD']['isotope']['order_export'] = array(
   'tables' => array('tl_order_export'),
   'icon'   => 'system/modules/contao_brand_manager/assets/icons/brand.png'
 );
+
+/* Models */
+$GLOBALS['TL_MODELS']['tl_order_export'] = 'Bcs\Model\OrderExport';
