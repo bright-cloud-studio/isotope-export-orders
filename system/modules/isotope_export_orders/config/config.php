2212240780873ef750b8c601c9e5b9cacca525fe
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-13 14:20:49 -0500

    Create config.php

---
+++ system/modules/isotope_export_orders/config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['BE_MOD']['isotope']['order_export'] = array(
+  'tables' => array('tl_order_export'),
+  'icon'   => 'system/modules/contao_brand_manager/assets/icons/brand.png'
+);
