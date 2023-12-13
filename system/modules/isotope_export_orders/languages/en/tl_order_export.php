2a3e9f7f8507c163b032a1a03572b7cbf60f7057
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-13 14:33:57 -0500

    Create tl_order_export.php

---
+++ system/modules/isotope_export_orders/languages/en/tl_order_export.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_brand']['brand_legend'] 	= 'Brand Values';
+$GLOBALS['TL_LANG']['tl_brand']['header_legend'] 	= 'Header Values';
+
+/* Unique Fields */
+$GLOBALS['TL_LANG']['tl_order_export']['config_name']  = array('Configuration Name', 'Enter the name for this configuration');
+$GLOBALS['TL_LANG']['tl_order_export']['csv_filename'] = array('CSV Filename', 'Enter the filename for the exported CSV file');
+
+/* Buttons */
+$GLOBALS['TL_LANG']['tl_order_export']['new']   		 	= array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_order_export']['show']  		 	= array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_order_export']['edit']  		 	= array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_order_export']['copy']  		 	= array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_order_export']['delete'] 			= array('Delete record', 'Delete record ID %s');
