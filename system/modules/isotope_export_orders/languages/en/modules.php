9766f528814e4cf19d2288f4014046776e496406
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-14 13:58:03 -0500

    Update modules.php

--- system/modules/isotope_export_orders/languages/en/modules.php
+++ system/modules/isotope_export_orders/languages/en/modules.php
@@ -1,4 +1,13 @@
 <?php
 
+/*
+ * Bright Cloud Studio's Isotope Export Orders
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-export-orders
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
 /* Back end modules */
 $GLOBALS['TL_LANG']['MOD']['order_export'] = array('Configure Order Exports', 'Customize the available options when exporting Orders');
