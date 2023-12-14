75411b73b09bfc8414a529e15718e5f2cdef0555
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-14 14:59:56 -0500

    Update tl_order_export.php

--- system/modules/isotope_export_orders/dca/tl_order_export.php
+++ system/modules/isotope_export_orders/dca/tl_order_export.php
@@ -83,7 +83,7 @@ $GLOBALS['TL_DCA']['tl_order_export'] = array
     // Palettes
     'palettes' => array
     (
-        'default'                     => '{order_export_legend},config_name,csv_filename,include_headers;{order_legend},order_id, order_number, order_status, payment_date, shipped_date, order_status, payment_method, shipping_method, subtotal, subtotal_without_tax, total, total_without_tax, currency, order_notes;{billing_address_legend},billing_first_name, billing_last_name, billing_date_of_birth, billing_company, billing_street_1, billing_street_2, billing_street_3, billing_zip, billing_city, billing_state, billing_country, billing_phone, billing_email;{shipping_address_legend},shipping_first_name, shipping_last_name, shipping_date_of_birth, shipping_company, shipping_street_1, shipping_street_2, shipping_street_3, shipping_zip, shipping_city, shipping_state, shipping_country, shipping_phone, shipping_email;{product_legend},product_id, product_name, product_sku, product_price, product_quantity, product_total;'
+        'default'                     => '{order_export_legend},config_name,csv_filename,include_headers;{order_legend},order_id, order_number, order_status, payment_date, shipped_date, payment_method, shipping_method, subtotal, subtotal_without_tax, total, total_without_tax, currency, order_notes;{billing_address_legend},billing_first_name, billing_last_name, billing_date_of_birth, billing_company, billing_street_1, billing_street_2, billing_street_3, billing_zip, billing_city, billing_state, billing_country, billing_phone, billing_email;{shipping_address_legend},shipping_first_name, shipping_last_name, shipping_date_of_birth, shipping_company, shipping_street_1, shipping_street_2, shipping_street_3, shipping_zip, shipping_city, shipping_state, shipping_country, shipping_phone, shipping_email;{product_legend},product_id, product_name, product_sku, product_price, product_quantity, product_total;'
     ),
     // Fields
     'fields' => array
@@ -164,13 +164,6 @@ $GLOBALS['TL_DCA']['tl_order_export'] = array
         'shipped_date' => array
         (
             'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipped_date'],
-            'inputType'               => 'checkbox',
-            'eval'                    => array('tl_class'=>'w50'),
-            'sql'                     => "char(1) NOT NULL default ''"
-        ),
-        'order_status' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['order_status'],
             'inputType'               => 'checkbox',
             'eval'                    => array('tl_class'=>'w50'),
             'sql'                     => "char(1) NOT NULL default ''"
