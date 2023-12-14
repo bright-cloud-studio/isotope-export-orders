9923396bfaf30bae7f8a2b849338c3393f7a09b5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-14 14:50:19 -0500

    Update tl_order_export.php

--- system/modules/isotope_export_orders/dca/tl_order_export.php
+++ system/modules/isotope_export_orders/dca/tl_order_export.php
@@ -126,6 +126,11 @@ $GLOBALS['TL_DCA']['tl_order_export'] = array
             'eval'                    => array('tl_class'=>'w50'),
             'sql'                     => "char(1) NOT NULL default ''"
         ),
+
+
+
+
+        
         // ORDER SETTINGS
         'order_id' => array
         (
@@ -148,6 +153,284 @@ $GLOBALS['TL_DCA']['tl_order_export'] = array
             'eval'                    => array('tl_class'=>'w50'),
             'sql'                     => "char(1) NOT NULL default ''"
         ),
+
+        'payment_date' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['payment_date'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipped_date' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipped_date'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'order_status' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['order_status'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'payment_method' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['payment_method'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_method' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_method'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'subtotal' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['subtotal'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'subtotal_without_tax' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['subtotal_without_tax'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'total' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['total'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'total_without_tax' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['total_without_tax'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'currency' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['currency'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'order_notes' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['order_notes'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        
+
+
+
+
+
+        // BILLING ADDRESS SETTINGS
+        'billing_first_name' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_first_name'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_last_name' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_last_name'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_date_of_birth' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_date_of_birth'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_company' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_company'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_street_1' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_street_1'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_street_2' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_street_2'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_street_3' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_street_3'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_zip' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_zip'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_city' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_city'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_state' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_state'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_country' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_country'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_phone' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_phone'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'billing_email' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['billing_email'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+
+
+
+
+        // SHIPPING ADDRESS SETTINGS
+        'shipping_first_name' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_first_name'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_last_name' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_last_name'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_date_of_birth' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_date_of_birth'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_company' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_company'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_street_1' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_street_1'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_street_2' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_street_2'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_street_3' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_street_3'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_zip' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_zip'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_city' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_city'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_state' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_state'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_country' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_country'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_phone' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_phone'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'shipping_email' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['shipping_email'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+
+
+        
+
+
+        
         // PRODUCT SETTINGS
         'product_id' => array
         (
@@ -170,9 +453,23 @@ $GLOBALS['TL_DCA']['tl_order_export'] = array
             'eval'                    => array('tl_class'=>'w50'),
             'sql'                     => "char(1) NOT NULL default ''"
         ),
+        'product_price' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['product_price'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
         'product_quantity' => array
         (
             'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['product_quantity'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "char(1) NOT NULL default ''"
+        ),
+        'product_total' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_order_export']['product_total'],
             'inputType'               => 'checkbox',
             'eval'                    => array('tl_class'=>'w50'),
             'sql'                     => "char(1) NOT NULL default ''"
