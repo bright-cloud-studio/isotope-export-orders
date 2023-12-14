a99c6da4f3d740b6d19062353b0cc94751136cbb
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-14 13:54:41 -0500

    Update config.php

--- system/modules/isotope_export_orders/config/config.php
+++ system/modules/isotope_export_orders/config/config.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * Bright Cloud Studio's Isotope Export Orders
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-export-orders
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
+/* Backend Modules */
 $GLOBALS['BE_MOD']['isotope']['order_export'] = array(
   'tables' => array('tl_order_export'),
   'icon'   => 'system/modules/contao_brand_manager/assets/icons/brand.png'
