f53ff52a161bd474d60874e653f2a139defe1fd1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-14 13:59:21 -0500

    Update tl_order_export.php

--- system/modules/isotope_export_orders/languages/en/tl_order_export.php
+++ system/modules/isotope_export_orders/languages/en/tl_order_export.php
@@ -1,29 +1,38 @@
 <?php
 
+/*
+ * Bright Cloud Studio's Isotope Export Orders
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-export-orders
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
 /* Legends */
 $GLOBALS['TL_LANG']['tl_order_export']['order_export_legend'] 	= 'Export Configuration Settings';
 $GLOBALS['TL_LANG']['tl_order_export']['order_legend'] 	= 'Order Settings';
 $GLOBALS['TL_LANG']['tl_order_export']['product_legend'] 	= 'Product Settings';
 
 /* Config Fields */
-$GLOBALS['TL_LANG']['tl_order_export']['config_name']  = array('Configuration Name', 'Enter the name for this configuration');
-$GLOBALS['TL_LANG']['tl_order_export']['csv_filename'] = array('CSV Filename', 'Enter the filename for the exported CSV file');
-$GLOBALS['TL_LANG']['tl_order_export']['include_headers'] = array('Include Headers', 'Include a header row in the exported CSV file');
+$GLOBALS['TL_LANG']['tl_order_export']['config_name']         = array('Configuration Name', 'Enter the name for this configuration');
+$GLOBALS['TL_LANG']['tl_order_export']['csv_filename']        = array('CSV Filename', 'Enter the filename for the exported CSV file');
+$GLOBALS['TL_LANG']['tl_order_export']['include_headers']     = array('Include Headers', 'Include a header row in the exported CSV file');
 
 /* Order Fields */
-$GLOBALS['TL_LANG']['tl_order_export']['order_id']      = array('Order ID', 'Include the "Order ID" field in the exported file');
-$GLOBALS['TL_LANG']['tl_order_export']['order_number']  = array('Order Number', 'Include the "Order Number" field in the exported file');
-$GLOBALS['TL_LANG']['tl_order_export']['order_status']  = array('Order Status', 'Include the "Order Status" field in the exported file');
+$GLOBALS['TL_LANG']['tl_order_export']['order_id']            = array('Order ID', 'Include the "Order ID" field in the exported file');
+$GLOBALS['TL_LANG']['tl_order_export']['order_number']        = array('Order Number', 'Include the "Order Number" field in the exported file');
+$GLOBALS['TL_LANG']['tl_order_export']['order_status']        = array('Order Status', 'Include the "Order Status" field in the exported file');
 
 /* Product Fields */
-$GLOBALS['TL_LANG']['tl_order_export']['product_id']      = array('Product ID', 'Include the "Product ID" field in the exported file');
-$GLOBALS['TL_LANG']['tl_order_export']['product_name']      = array('Product Name', 'Include the "Product Name" field in the exported file');
-$GLOBALS['TL_LANG']['tl_order_export']['product_sku']      = array('Product SKU', 'Include the "Product SKU" field in the exported file');
-$GLOBALS['TL_LANG']['tl_order_export']['product_quantity']      = array('Product Quantity', 'Include the "Product Quantity" field in the exported file');
+$GLOBALS['TL_LANG']['tl_order_export']['product_id']          = array('Product ID', 'Include the "Product ID" field in the exported file');
+$GLOBALS['TL_LANG']['tl_order_export']['product_name']        = array('Product Name', 'Include the "Product Name" field in the exported file');
+$GLOBALS['TL_LANG']['tl_order_export']['product_sku']         = array('Product SKU', 'Include the "Product SKU" field in the exported file');
+$GLOBALS['TL_LANG']['tl_order_export']['product_quantity']    = array('Product Quantity', 'Include the "Product Quantity" field in the exported file');
 
 /* Buttons */
-$GLOBALS['TL_LANG']['tl_order_export']['new']   		 	= array('New record', 'Add a new record');
-$GLOBALS['TL_LANG']['tl_order_export']['show']  		 	= array('Record details', 'Show the details of record ID %s');
-$GLOBALS['TL_LANG']['tl_order_export']['edit']  		 	= array('Edit record', 'Edit record ID %s');
-$GLOBALS['TL_LANG']['tl_order_export']['copy']  		 	= array('Copy record', 'Copy record ID %s');
-$GLOBALS['TL_LANG']['tl_order_export']['delete'] 			= array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_order_export']['new']   		 	  = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_order_export']['show']  		      = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_order_export']['edit']  		 	  = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_order_export']['copy']  		 	  = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_order_export']['delete'] 			  = array('Delete record', 'Delete record ID %s');
