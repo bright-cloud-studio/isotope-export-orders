c7aef24d74f68d3aba9f1fc656c98d7b78284f7f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-14 14:02:21 -0500

    Update OrderExport.php

--- system/modules/isotope_export_orders/library/Bcs/Model/OrderExport.php
+++ system/modules/isotope_export_orders/library/Bcs/Model/OrderExport.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Bright Cloud Studio's Isotope Export Orders
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-export-orders
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
 namespace Bcs\Model;
 
 class OrderExport extends \Model
