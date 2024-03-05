be252844198bb50ccc145a3fa94e0bd4a4df657c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-03-05 13:39:49 -0500

    Update tl_iso_product_collection.php

--- system/modules/isotope_export_orders/dca/tl_iso_product_collection.php
+++ system/modules/isotope_export_orders/dca/tl_iso_product_collection.php
@@ -15,6 +15,7 @@ use Bcs\Backend\DcaCallback;
 /* Callbacks */
 $GLOBALS['TL_DCA']['tl_iso_product_collection']['config']['onload_callback'][] = [DcaCallback::class, 'onLoad'];
 $GLOBALS['TL_DCA']['tl_iso_product_collection']['select']['buttons_callback'][] = [ButtonCallback::class, 'addExportButton'];
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['label']['fields']['label_callback'] = array('Bcs\Backend\OrderExporter', 'getOrderLabel');
 
 
  /* Extend the tl_user palettes */
